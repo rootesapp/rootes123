d60609e604b9c3db788db026ee715c9cc398977e
rootesapp <htmlhtml5@foxmail.com>
2024-11-01 23:02:06 +0800

    Update MainActivity.kt

--- app/src/main/java/com/root/system/MainActivity.kt
+++ app/src/main/java/com/root/system/MainActivity.kt
@@ -51,7 +51,7 @@ class MainActivity : AppCompatActivity() {
     private var isDialogShown = false
     override fun onCreate(savedInstanceState: Bundle?) {
       
-   Toast.makeText(this, "正在使用玩机百宝箱开源版", Toast.LENGTH_LONG).show()
+   Toast.makeText(this, "Powered by 玩机百宝箱", Toast.LENGTH_LONG).show()
         
         super.onCreate(savedInstanceState)
         ThemeModeState.switchTheme(this)
